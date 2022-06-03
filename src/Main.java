184380267d75ab7ef9e4aaf5f0df3b3e70e20b75
Budina Anastasiya <abezusparis@gmail.com>
2022-06-03 20:06:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int price = 10_200;
+
+        int miles = price / 20;
+        System.out.println("Бонусные мили : " + miles);
+
+    }
+}
